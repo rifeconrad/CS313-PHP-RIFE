a3dc8c35653cd109ef9a133054b74497181619fb
rifeconrad <rif16001@byui.edu>
2019-04-26 16:25:37 -0400

    DEBUG

--- web/home.js
+++ web/home.js
@@ -2,6 +2,7 @@ document.getElementById("img_btn").addEventListener("click", toggleImg);
 
 var toggle = false;
 function toggleImg() {
+	console.log("SHOWING");
 	if (toggle) {
 		document.getElementById("img_view").style.visibility = "none";
 		toggle = true;
