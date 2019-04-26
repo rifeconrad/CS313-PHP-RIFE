641956cb5e83ec1b8a57fc59d91582a05423560e
rifeconrad <rif16001@byui.edu>
2019-04-26 13:05:41 -0400

    zzzz

--- web/Teach/01/script.js
+++ web/Teach/01/script.js
@@ -8,8 +8,7 @@ function changeColor() {
 	document.getElementById("first").style.backgroundColor = color;
 }
 
-$(document).read(function(){
-	$("#change").click(function() {
-		$("#first").css("background-color", $("#color").val());
-	});
+
+$("#change").click(function() {
+	$("#first").css("background-color", $("#color").val());
 });
