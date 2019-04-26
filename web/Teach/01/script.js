43c6ebdcde39974134fe7232bf39178bbb4e26cd
rifeconrad <rif16001@byui.edu>
2019-04-26 12:42:33 -0400

    changing colorsnow!

--- web/Teach/01/script.js
+++ web/Teach/01/script.js
@@ -2,3 +2,8 @@ function clicked()
 {
 	alert("Clicked!");
 }
+
+function changeColor() {
+	var color = document.getElementById("color").value;
+	document.getElementById("first").style.color = color;
+}
