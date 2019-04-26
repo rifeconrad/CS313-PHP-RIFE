0c79fc8ce4eb92bb27323f1fef838c861fe94671
rifeconrad <rif16001@byui.edu>
2019-04-26 12:51:53 -0400

    testing jquery

--- web/Teach/01/script.js
+++ web/Teach/01/script.js
@@ -7,3 +7,9 @@ function changeColor() {
 	var color = document.getElementById("color").value;
 	document.getElementById("first").style.backgroundColor = color;
 }
+
+$(document).read(function(){
+	$(#change).click(change() {
+		$(#first).css("background-color: " + #(color).val());
+	};
+});
