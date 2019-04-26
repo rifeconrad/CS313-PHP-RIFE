9776115b12b0b5c3996148b17437d979c8786fff
rifeconrad <rif16001@byui.edu>
2019-04-26 16:28:37 -0400

    changed names

--- web/home.js
+++ web/home.js
@@ -1,13 +1,5 @@
-document.getElementById("img_btn").addEventListener("click", toggleImg);
+document.getElementById("img_btn").addEventListener("click", viewImg);
 
-var toggle = false;
-function toggleImg() {
-	console.log("SHOWING");
-	if (toggle) {
-		document.getElementById("img_view").style.display = "none";
-		toggle = true;
-	} else {
-		document.getElementById("img_view").style.display = "initial";
-		toggle = false;
-	}
+function viewImg() {
+	document.getElementById("img_view").style.display = "initial";
 }
