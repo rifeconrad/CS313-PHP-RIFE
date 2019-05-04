2d477e246f4d51d7c6bad5ae2eba5b35844b3e86
rifeconrad <rif16001@byui.edu>
2019-05-04 06:31:20 -0400

    hmm

--- web/home.php
+++ web/home.php
@@ -3,6 +3,8 @@
 <head>
 	<title>Conrad's CS313 Home Page</title>
 	<link rel="stylesheet" type="text/css" href="home.css">
+	
+	<script src="home.js"></script>
 </head>
 <body>
 	<div id="main_container">
@@ -15,6 +17,7 @@
 			<p>
 				I was born in California, but was raised in Ohio. I love spending<br> time with my family, playing board games, programming, sleeping,<br>going to work, playing video games, and listening to music. My most<br> recent hobby is listening to and memorizing German songs with my<br> wife. 
 			</p>
+
 			<div id="img_btn">VIEW FAMILY PICTURE</div>
 			<div id="img_view"><img src="images/family_picture.jpg" alt="family_picture"></div>
 			<?php echo "<p>" . "System Time: " . date("D M d, Y G:i a") . "</p>"; ?>
@@ -44,6 +47,5 @@
 			?>
 		</div>
 	</div>
-	<script src="home.js"></script>
 </body>
 </html>
