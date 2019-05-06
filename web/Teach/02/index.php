e18a021d61747bcf5e0555e01f97caf8d44dc611
rifeconrad <rif16001@byui.edu>
2019-05-06 13:53:30 -0400

    dasfasdf

--- web/Teach/02/index.php
+++ web/Teach/02/index.php
@@ -15,11 +15,37 @@
 
 		<input type="checkbox" name="continent[]" value="North America">North America<br>
 		<input type="checkbox" name="continent[]" value="South America">South America<br>
-		<input type="checkbox" name="continent[]" value="EU">Europe<br>
-		<input type="checkbox" name="continent[]" value="ASIA">Asia<br>
-		<input type="checkbox" name="continent[]" value="AUS">Australia<br>
-		<input type="checkbox" name="continent[]" value="AFR">Africa<br>
-		<input type="checkbox" name="continent[]" value="ANT">Antarctica<br>
+		<input type="checkbox" name="continent[]" value="Europe">Europe<br>
+		<input type="checkbox" name="continent[]" value="Asia">Asia<br>
+		<input type="checkbox" name="continent[]" value="Australia">Australia<br>
+		<input type="checkbox" name="continent[]" value="Africa">Africa<br>
+		<input type="checkbox" name="continent[]" value="Antarctica">Antarctica<br>
+
+		Comment: <textarea name="comments"></textarea><br>
+		<input type="submit">
+	</form>
+
+	<hr>
+
+	<form action="input.php" method="post">
+		Name: <input type="text" name="name"><br>
+		E-mail: <input type="text" name="email"><br>
+
+		<?php
+			$majors = array("Computer Science", "Web Design", "CIT", "CE");
+
+			foreach($majors as $major) {
+				echo "<input type='radio' name='major' value='$major'>$major<br>"
+			}
+		?>
+
+		<input type="checkbox" name="continent[]" value="North America">North America<br>
+		<input type="checkbox" name="continent[]" value="South America">South America<br>
+		<input type="checkbox" name="continent[]" value="Europe">Europe<br>
+		<input type="checkbox" name="continent[]" value="Asia">Asia<br>
+		<input type="checkbox" name="continent[]" value="Australia">Australia<br>
+		<input type="checkbox" name="continent[]" value="Africa">Africa<br>
+		<input type="checkbox" name="continent[]" value="Antarctica">Antarctica<br>
 
 		Comment: <textarea name="comments"></textarea><br>
 		<input type="submit">
