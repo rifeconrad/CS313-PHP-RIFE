7c86cde6d76bb78777f262320ead64f53d23d180
rifeconrad <rif16001@byui.edu>
2019-05-06 13:24:51 -0400

    addsaf

--- web/Teach/02/index.php
+++ web/Teach/02/index.php
@@ -4,15 +4,16 @@
 	<title>Teach 02</title>
 </head>
 <body>
-	<form method="post">
+	<form action="input.php" method="post">
 		Name: <input type="text" name="name"><br>
 		E-mail: <input type="text" name="email"><br>
 		<input type="radio" name="major" value="CS"> Computer Science<br>
 		<input type="radio" name="major" value="WDD"> Web Design and Development<br>
 		<input type="radio" name="major" value="CIT"> Computer Information Technology<br>
 		<input type="radio" name="major" value="CE"> Computer Engineering<br>
+		Comment: <textarea></textarea><br>
 		<input type="submit">
-		Comment: <textarea></textarea>
+
 	</form>
 </body>
 </html>
