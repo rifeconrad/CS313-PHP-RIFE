d4233987bebdac344cb2eaa50a851310f8dc6dd9
rifeconrad <rif16001@byui.edu>
2019-05-07 13:44:15 -0400

    prove commit

--- web/Prove/03/cart.php
+++ web/Prove/03/cart.php
@@ -6,9 +6,9 @@
 <body>
 	<script>
 		var items = ["Food", "Cat", "Dog", "Fish"];
-		for (item in items) {
-			console.log(item);
-			var item_str = sessionStorage[item];
+		for (i in items) {
+			console.log(items[i]);
+			var item_str = sessionStorage[items[i]];
 			console.log("ITEM STRING " + item_str);
 			var item = JSON.parse(item_str);
 			document.innerHTML = item.name + " " + item.quantity + " " + item.price + "<br>";
