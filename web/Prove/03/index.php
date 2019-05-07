f5280eec9575324f1bdff215d1b46b2c0a8ab4f8
rifeconrad <rif16001@byui.edu>
2019-05-07 14:16:58 -0400

    prove commit

--- web/Prove/03/index.php
+++ web/Prove/03/index.php
@@ -7,21 +7,16 @@
 </head>
 <body>
 	<?php
-		$times = 0;
-		if ($_POST) {
-			$times++;
-			echo "$times";
-		}
+		$item = "";
+		echo $item;
 	?>
 
-	<form action="" name="browse" method="post">
-		Apple<input type='text' maxlength='2' size='2'/><button id='apple_btn' value='Apple'>Add To Cart</button><br>
+	Apple<input type='text' maxlength='2' size='2'/><button id='apple_btn' value='Apple'>Add To Cart</button><br>
 
-		Banana<input type='text' maxlength='2' size='2'/><button id='banana_btn' value='Banana'>Add To Cart</button><br>
+	Banana<input type='text' maxlength='2' size='2'/><button id='banana_btn' value='Banana' onclick="<?php echo('$item');?>">Add To Cart</button><br>
 
-		Orange<input type='text' maxlength='2' size='2'/><button id='orange_btn' value='Orange'>Add To Cart</button><br>
+	Orange<input type='text' maxlength='2' size='2'/><button id='orange_btn' value='Orange'>Add To Cart</button><br>
 
-		Grape<input type='text' maxlength='2' size='2'/><button id='grape_btn' value='Grape'>Add To Cart</button><br>
-	</form>
+	Grape<input type='text' maxlength='2' size='2'/><button id='grape_btn' value='Grape'>Add To Cart</button><br>
 </body>
 </html>
