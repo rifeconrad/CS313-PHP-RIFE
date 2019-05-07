540fc9196036620d0df6b238eb57207eed804047
rifeconrad <rif16001@byui.edu>
2019-05-07 12:24:48 -0400

    prove commit

--- web/Prove/03/header.php
+++ web/Prove/03/header.php
@@ -1,7 +1,7 @@
 <?php
 	echo "<div id='nav'>
 			<a id='here' href='index.php'>HOME</a>
-			<a onclick='document.getElementById('browse').submit();'>CART</a>
+			<a onclick='document.getElementById('browse').submit()'>CART</a>
 			<a href='checkout.php'>CHECKOUT</a> 
 		  </div>";
 ?>
