3cc33124bf7fed3b95a989668f0fb5fb0b0267af
rifeconrad <rif16001@byui.edu>
2019-05-07 11:42:40 -0400

    prove commit

--- web/Prove/03/index.php
+++ web/Prove/03/index.php
@@ -5,5 +5,17 @@
 </head>
 <body>
 	<?php include 'header.php';?>
+
+	<form action="input.php" method="post">
+		<?php
+			$items = array("Food", "Cat", "Dog", "Fish");
+
+			foreach($items as $item) {
+				echo "<input type='checkbox' name='item' value='$item'>$item<br>";
+			}
+		?>
+		<input type="submit">
+
+	</form>
 </body>
 </html>
