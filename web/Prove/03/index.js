ce672e731b9e7dd20a9f8dfec85e2deb767bdcca
rifeconrad <rif16001@byui.edu>
2019-05-07 12:38:08 -0400

    prove commit

--- web/Prove/03/index.js
+++ web/Prove/03/index.js
@@ -1,9 +1,3 @@
-window.addEventListener('load', init);
-
-function init() {
-	document.getElementById("cart_btn").addEventListener("click", showCart, false);
-}
-
 function showCart() {
 	document.getElementById("browse").submit();
 }
