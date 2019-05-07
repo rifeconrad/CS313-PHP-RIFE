2edf81e5c7f26bb9f6abd4addd68e37dfe57fa1f
rifeconrad <rif16001@byui.edu>
2019-05-07 11:37:34 -0400

    prove commit

---
+++ web/Prove/03/header.php
@@ -0,0 +1,7 @@
+<?php
+	echo "<div id='nav'>
+			<a id='here' href='index.php'>HOME</a>
+			<a href='cart.php'>CART</a>
+			<a href='checkout.php'>CHECKOUT</a> 
+		  </div>"
+?>
---
+++ web/Prove/03/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prove 03 Store</title>
+</head>
+<body>
+	<?php include 'header.php';?>
+</body>
+</html>
