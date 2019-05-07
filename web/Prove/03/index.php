6d5f457ffe70d6f7bd62cd9b8cc59ccf25c63083
rifeconrad <rif16001@byui.edu>
2019-05-07 14:18:26 -0400

    prove commit

--- web/Prove/03/index.php
+++ web/Prove/03/index.php
@@ -8,12 +8,11 @@
 <body>
 	<?php
 		$item = "5";
-		echo $item;
 	?>
 
 	Apple<input type='text' maxlength='2' size='2'/><button id='apple_btn' value='Apple'>Add To Cart</button><br>
 
-	Banana<input type='text' maxlength='2' size='2'/><button id='banana_btn' value='Banana' onclick="<?php echo('$item');?>">Add To Cart</button><br>
+	Banana<input type='text' maxlength='2' size='2'/><button id='banana_btn' value='Banana' href="<?php echo('$item');?>">Add To Cart</button><br>
 
 	Orange<input type='text' maxlength='2' size='2'/><button id='orange_btn' value='Orange'>Add To Cart</button><br>
 
