6c91f22e228e06531feb4b434ac52f7479af236e
rifeconrad <rif16001@byui.edu>
2019-05-07 13:37:12 -0400

    prove commit

--- web/Prove/03/cart.php
+++ web/Prove/03/cart.php
@@ -6,7 +6,7 @@
 <body>
 	<script>
 		for (item_str in sessionStorage) {
-
+			console.log(sessionStorage[item_str]);
 			var item = JSON.parse(sessionStorage[item_str]);
 			document.innerHTML = item.name + " " + item.quantity + " " + item.price + "<br>";
 		}
