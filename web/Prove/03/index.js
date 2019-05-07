28188b7ce967030358ecd11168161011a343edbf
rifeconrad <rif16001@byui.edu>
2019-05-07 13:06:52 -0400

    prove commit

--- web/Prove/03/index.js
+++ web/Prove/03/index.js
@@ -1,7 +1,25 @@
-function showCart() {
-	document.getElementById("browse").submit();
+window.addEventListener('load', init);
+
+function init() {
+	document.getElementById("item_btn").addEventListener("click", addToCart, false);
 }
 
-function showCheckout() {
-	document.getElementById("checkout").submit();
+function Item(name, quantity, price)
+{
+	this.name = name;
+	this.quantity = quantity;
+	this.price = price;
+}
+function addToCart() {
+	// grab data
+	var item_name = document.getElementById("item_btn").value;
+	var qID = "q" + item_name;
+	var item_quantity = document.getElementById(qID).value;
+
+	// create object and stringify data
+	var item = new Item(item_name, item_quantity, 10);
+	var item_str = JSON.stringify(item);
+
+ 	// store data to session
+	sessionStorage.setItem(item_name, item_str);
 }
