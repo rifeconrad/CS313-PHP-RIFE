ac9f8f278de105bacbb77aed7415482d1fd55417
rifeconrad <rif16001@byui.edu>
2019-05-07 12:42:13 -0400

    prove commit

--- web/Prove/03/index.php
+++ web/Prove/03/index.php
@@ -6,18 +6,19 @@
 	<script src="index.js"></script>
 </head>
 <body>
-	<form id="checkout" action="checkout.php" method="post"></form>
-	<form id="browse" action="cart.php" method="post">
-		<?php
-			include 'header.php';
+	<form id="checkout" action="checkout.php" method="post">
+		<form id="browse" action="cart.php" method="post">
+			<?php
+				include 'header.php';
 
-			$items = array("Food", "Cat", "Dog", "Fish");
+				$items = array("Food", "Cat", "Dog", "Fish");
 
-			foreach($items as $item) {
-				echo "<input type='checkbox' name='item[]' value='$item'>$item<input type='text' name='q$item' maxlength='2' size='2'/><br>";
-			}
-		?>
-		<button>Go To Cart</button>
+				foreach($items as $item) {
+					echo "<input type='checkbox' name='item[]' value='$item'>$item<input type='text' name='q$item' maxlength='2' size='2'/><br>";
+				}
+			?>
+			<button>Go To Cart</button>
+		</form>
 	</form>
 
 </body>
