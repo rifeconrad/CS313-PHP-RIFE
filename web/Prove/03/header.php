aa411e956fa00e19f0ee418e23ad43d192a75eee
rifeconrad <rif16001@byui.edu>
2019-05-07 13:27:59 -0400

    prove commit

--- web/Prove/03/header.php
+++ web/Prove/03/header.php
@@ -1,7 +1,7 @@
 <?php
 	echo "<div id='nav'>
 			<a href='index.php'>HOME</a>
-			<a href='javascript: showCart()'>CART</a>
+			<a href='cart.php'>CART</a>
 			<a href='javascript: showCheckout()'>CHECKOUT</a> 
 		  </div>";
 ?>
