f4b6719d42cd8e479babd3a26b4be8ba4c3bba77
rifeconrad <rif16001@byui.edu>
2019-05-08 06:04:35 -0400

    prove commit

--- web/Prove/03/cart.php
+++ web/Prove/03/cart.php
@@ -2,12 +2,10 @@
 	include("header.php"); 
 	include("fake_database.php");
 
-	if (!isset($_SESSION["cart"])) {
+	if (isset($_SESSION["cart"])) {
 		echo count($_SESSION["cart"]);
     	foreach ($cart as $item) {
     		echo "<p>" . $item->name . " " . $item->price . "</p>";
     	}
-  	} else {
-  		echo "NOPE";
-  	}
+  	} 
 ?>
