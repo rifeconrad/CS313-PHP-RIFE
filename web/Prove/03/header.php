339c52d29960a7071a4e5efe2091646d5efbea60
rifeconrad <rif16001@byui.edu>
2019-05-08 05:50:48 -0400

    prove commit

--- web/Prove/03/header.php
+++ web/Prove/03/header.php
@@ -8,7 +8,7 @@
 ?>
 
 <div id='nav'>
-	<a href='index.php?active=HOME' id="<?php echo ($active === "HOME" ? "active_link" : "") ?>">HOME</a>
-	<a href='cart.php?active=CART' id="<?php echo ($active === "CART" ? "active_link" : "") ?>">CART</a>
-	<a href='checkout.php?active=CHECKOUT' id="<?php echo ($active === "CHECKOUT" ? "active_link" : "") ?>">CHECKOUT</a> 
+	<a href='index.php?active=HOME' id="<?php echo ($active === "HOME" ? "active_link" : "non_active_link") ?>">HOME</a>
+	<a href='cart.php?active=CART' id="<?php echo ($active === "CART" ? "active_link" : "non_active_link") ?>">CART</a>
+	<a href='checkout.php?active=CHECKOUT' id="<?php echo ($active === "CHECKOUT" ? "active_link" : "non_active_link") ?>">CHECKOUT</a> 
 </div>
