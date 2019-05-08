c53cd881bf4d0af96bb197e09e97a6fbbbdd498e
rifeconrad <rif16001@byui.edu>
2019-05-08 05:43:40 -0400

    prove commit

--- web/Prove/03/header.php
+++ web/Prove/03/header.php
@@ -5,21 +5,10 @@
 	if (isset($_GET["active"])) {
 		$active = $_GET["active"];
 	}
-
-	switch ($active) {
-		case "HOME":
-		break;
-		case "CART":
-		break;
-		case "CHECKOUT":
-		break;
-		default:
-		break;
-	}
 ?>
 
 <div id='nav'>
-	<a href='index.php?active=HOME'>HOME</a>
-	<a href='cart.php?active=CART'>CART</a>
-	<a href='checkout.php?active=CHECKOUT'>CHECKOUT</a> 
+	<a href='index.php?active=HOME' id="<?php echo ($active === "HOME" ? "active_link" : "") ?>">HOME</a>
+	<a href='cart.php?active=CART' id="<?php echo ($active === "CART" ? "active_link" : "") ?>">CART</a>
+	<a href='checkout.php?active=CHECKOUT' id="<?php echo ($active === "CHECKOUT" ? "active_link" : "") ?>">CHECKOUT</a> 
 </div>
