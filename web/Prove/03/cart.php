6b5ac83faac6efa1145c796f9f5c007cc2de1800
rifeconrad <rif16001@byui.edu>
2019-05-08 06:39:40 -0400

    prove commit

--- web/Prove/03/cart.php
+++ web/Prove/03/cart.php
@@ -3,12 +3,30 @@
 	include("header.php"); 
 	include("fake_database.php");
 
+	if (isset($_GET["remove"])) {
+		$product_name = $_GET["remove"];
+
+		if (isset($_SESSION["cart"])) {
+    		foreach ($_SESSION["cart"] as $item) {
+    			if ($item === $product_name) {
+    				unset($_SESSION["cart"][$item]);
+    			}
+    		}
+    	}
+	}
+
 	if (isset($_SESSION["cart"])) {
     	foreach ($_SESSION["cart"] as $item) {
     		if (isset($item->name)) {
-    			echo "<p>" . $item->name . " " . $item->price . "</p>";
-    		} else {
-    			echo "ERROR";
+?>
+
+	<div id="cart_display">
+		<div id="name"> <?php echo $item->name; ?> </div>
+		<div id="price"> <?php echo $item->price; ?> </div>
+		<a href="cart.php?remove=<?php echo $item->name; ?>">Remove Item</a>
+    </div>
+
+<?php
     		}
     	}
   	} 
