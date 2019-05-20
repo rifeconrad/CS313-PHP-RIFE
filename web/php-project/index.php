99e05258159b42bfd99d7aa47a4db4b5763572c9
rifeconrad <rif16001@byui.edu>
2019-05-20 12:37:24 -0400

    test

--- web/php-project/index.php
+++ web/php-project/index.php
@@ -1,3 +1,4 @@
+<?php
 try
 {
 	$DATABASE_URL='postgres://cgojnkckkncmxf:e510ef76e9ff31ffd3069afa499c4d2bd748c1f2ea77e9da50f906dbe8f8c3e1@ec2-23-21-184-181.compute-1.amazonaws.com:5432/dfnorfi59oivff';
@@ -11,9 +12,12 @@ try
     // and can be very helpful in debugging! (But you would likely want
     // to disable it for production environments.)
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo 'SUCCESS!';
 }
 catch (PDOException $ex)
 {
     echo 'Error!: ' . $ex->getMessage();
     die();
 }
+?>
