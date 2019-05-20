afeac13972b4e3b2799aec76730eafa0cba3486e
rifeconrad <rif16001@byui.edu>
2019-05-20 12:52:24 -0400

    test

--- web/php-project/index.php
+++ web/php-project/index.php
@@ -14,6 +14,8 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  echo 'Success!';
 }
 catch (PDOException $ex)
 {
