3d4b3df71173df5db246610ae06c860313626d3a
rifeconrad <rif16001@byui.edu>
2019-05-20 12:48:01 -0400

    test

--- web/php-project/index.php
+++ web/php-project/index.php
@@ -1,13 +1,12 @@
 <?php
 try
 {
-	echo 'UPDATED';
-	$DATABASE_URL='enigmatic-ravine-85455';
+	$database_name='postgres://cgojnkckkncmxf:e510ef76e9ff31ffd3069afa499c4d2bd748c1f2ea77e9da50f906dbe8f8c3e1@ec2-23-21-184-181.compute-1.amazonaws.com:5432/dfnorfi59oivff';
 
 
   	$user = 'username';
  	$password = 'password';
-    $db = new PDO('pgsql:host=localhost;dbname=$DATABASE_URL', $user, $password);
+    $db = new PDO('pgsql:host=localhost;dbname=$database_name', $user, $password);
 
     // this line makes PDO give us an exception when there are problems,
     // and can be very helpful in debugging! (But you would likely want
