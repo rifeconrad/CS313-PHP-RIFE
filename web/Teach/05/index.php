e3eb69fd9b65caf699bf0d83ee9c2f7328bf1edf
rifeconrad <rif16001@byui.edu>
2019-05-20 13:18:07 -0400

    test

---
+++ web/Teach/05/index.php
@@ -0,0 +1,38 @@
+<?php
+	try
+	{
+	  $dbUrl = getenv('DATABASE_URL');
+
+	  $dbOpts = parse_url($dbUrl);
+
+	  $dbHost = $dbOpts["host"];
+	  $dbPort = $dbOpts["port"];
+	  $dbUser = $dbOpts["user"];
+	  $dbPassword = $dbOpts["pass"];
+	  $dbName = ltrim($dbOpts["path"],'/');
+
+	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	  echo 'Success!';
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+
+	echo "<h1>Scripture Resources</h1>";
+	/*John 3:16 - "For God so loved the world, 
+	that he gave his only begotten Son, that whosoever 
+	believeth in him should not perish, but have everlasting 
+	life."*/
+	
+	foreach ($db->query('SELECT * FROM scriptures') as $row)
+	{
+	  echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - " .
+	  $row['content'] . "<br>";
+	}
+	
+?>
