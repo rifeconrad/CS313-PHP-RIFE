b8ac0a8002318f5956b948dbf90801232a0ec434
rifeconrad <rif16001@byui.edu>
2019-05-21 13:48:52 -0400

    test

--- web/php-project/index.php
+++ web/php-project/index.php
@@ -1,25 +1,46 @@
 <?php
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+    $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  echo 'Success!';
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+    echo 'Success!';
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+
+  if (isset($_POST['uname'])) {
+    $username = $_POST['uname'];
+    $password = "";
+    foreach ($db->query('SELECT * FROM users') as $row)
+    {
+      if ($row['username'] == $username) {
+        $password = $row['password'];
+      }
+    }
+  }
+
+  if ($password != "") {
+    echo "Successful Login!";
+  } else {
+    if (isset($_POST['password'])) {
+      $password = $_POST['password'];
+
+      $db->query('INSERT INTO users (username, password) VALUES ($username, $password)');
+    }
+  }
 ?>
