85b15ef8b0fc3e2aa7ddca30e432af003ae0ab4c
rifeconrad <rif16001@byui.edu>
2019-05-22 04:59:18 -0400

    test

--- web/php-project/login.php
+++ web/php-project/login.php
@@ -1,27 +1,41 @@
-<?php
-  require 'db_runner.php';
 
-  if (isset($_GET['attempt'])) {  
-    if ($_GET['attempt'] == "fail") {
-?>
-      <p style="color:red;">Failed to login</p>
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hobby Hero's Login Page</title>
+</head>
+<body>
+
+  <?php
+    require 'db_runner.php';
+  ?>
+
+  <form action="index.php" method="post">
+    <div class="imgcontainer">
+      <img src="https://library.kissclipart.com/20180904/voe/kissclipart-cartoon-ww2-helmet-clipart-world-war-ii-combat-hel-fc2b99fd8748a2e4.jpg" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <button type="submit">Login</button>
+    </div>
+  </form>
+
+  <?php
+    if (isset($_GET['attempt'])) {  
+      if ($_GET['attempt'] == "fail") {
+  ?>
+        <p style="color:red;">Failed to login</p>
+  <?php
+      }
     }
-  }
-?>
+  ?>
 
-<form action="index.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
 
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit">Login</button>
-  </div>
-</form>
+</body>
+</html>
