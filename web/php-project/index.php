355de6f08b0c9289b44b3c95dccbf6533da9d1e5
rifeconrad <rif16001@byui.edu>
2019-05-22 10:07:16 -0400

    test

--- web/php-project/index.php
+++ web/php-project/index.php
@@ -3,9 +3,9 @@
 	require 'db_runner.php';
   session_start();
 
-	$username = "";
   	if (isset($_POST['uname'])) {
-  	    $username = htmlspecialchars($_POST['uname']);
+  	  $username = htmlspecialchars($_POST['uname']);
+      $_SESSION['username'] = $username
     	foreach ($db->query('SELECT * FROM users') as $row)
     	{
    	   		if ($row['username'] == $username && $_POST['psw'] ==  $row['password']) {
@@ -14,6 +14,9 @@
     	}
   	}
 
+  if (!isset($_SESSION['password_success'])) {
+    header('Location: login.php?attempt=fail'); 
+  }
   if (!$_SESSION['password_success']) {
   	header('Location: login.php?attempt=fail'); 
   }
@@ -28,7 +31,9 @@
 	<?php
 		include 'header.php';
 
-    	echo "Welcome " . $username;
+    if (isset($_SESSION['username'])) {
+    	echo "Welcome " . $_SESSION['username'];
+    }
 
 		foreach ($db->query('SELECT * FROM stock') as $row)
     	{
