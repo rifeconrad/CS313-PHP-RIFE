41277302df39bdd2eff88dbdcfa746af813df780
rifeconrad <rif16001@byui.edu>
2019-06-03 13:41:10 -0400

    commit

--- web/Teach/07/index.php
+++ web/Teach/07/index.php
@@ -8,6 +8,19 @@
 	<?php
 		include "db_runner.php";
 
+		if (isset($_POST['new_username']) && isset($_POST['new_password'])) {
+			$username = htmlspecialchars($_POST['new_username']);
+			$password = htmlspecialchars($_POST['new_password']);
+
+			if ($username != "" || $password != "") {
+				$db_insert = $db->prepare('INSERT INTO users(username, password) VALUES(:username, :password)');
+
+				$db_insert->bindValue(':username', $username, PDO::PARAM_STR);
+				$db_insert->bindValue(':password', $password, PDO::PARAM_STR);
+				$db_insert->execute();
+			}
+		}
+
 		if (isset($_POST['username']) && isset($_POST['password'])) {
 			$username = htmlspecialchars($_POST['username']);
 			$password = htmlspecialchars($_POST['password']);
@@ -33,7 +46,7 @@
 
 	<form action="index.php" method="POST">
 		<input type="text" name="username" placeholder="Username" required></input><br>
-		<input type="text" name="password" placeholder="Password" required></input><br>
+		<input type="password" name="password" placeholder="Password" required></input><br>
 		<button type="submit">Login</button>
 	</form>
 	<form action="signup.php" method="POST">
--- web/Teach/07/signup.php
+++ web/Teach/07/signup.php
@@ -8,14 +8,12 @@
 	<?php
 		require "db_runner.php";
 
-		if (isset($_POST['username']) && isset($_POST['password'])) {
-			
-		}
+
 	?>
 
 	<form action="index.php" method="POST">
-		<input type="text" name="username" placeholder="New Username" required></input><br>
-		<input type="text" name="password" placeholder="New Password" required></input><br>
+		<input type="text" name="new_username" placeholder="New Username" required></input><br>
+		<input type="text" name="new_password" placeholder="New Password" required></input><br>
 		<button type="submit">Login</button>
 	</form>
 
