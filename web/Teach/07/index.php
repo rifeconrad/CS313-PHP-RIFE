a90bcd2188278dcb45d5333fce96b96f14dfec04
rifeconrad <rif16001@byui.edu>
2019-06-03 13:09:38 -0400

    commit

---
+++ web/Teach/07/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teach 07</title>
+</head>
+<body>
+	<form action="index.php" method="POST">
+		<input type="text" name="username" placeholder="Username" required></input>
+		<input type="text" name="password" placeholder="password" required></input>
+		<button type="submit">Login</button>
+	</form>
+</body>
+</html>
