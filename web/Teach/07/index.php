59b608eb3a6e0542308f74330e5944a662ad67df
rifeconrad <rif16001@byui.edu>
2019-06-03 13:10:29 -0400

    commit

--- web/Teach/07/index.php
+++ web/Teach/07/index.php
@@ -6,7 +6,7 @@
 <body>
 	<form action="index.php" method="POST">
 		<input type="text" name="username" placeholder="Username" required></input><br>
-		<input type="text" name="password" placeholder="password" required></input><br>
+		<input type="text" name="password" placeholder="Password" required></input><br>
 		<button type="submit">Login</button>
 	</form>
 </body>
