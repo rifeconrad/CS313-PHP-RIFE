5df349640fefea2df8d2a061ceabdaa6167fefda
rifeconrad <rif16001@byui.edu>
2019-06-03 14:30:20 -0400

    commit

--- web/php-project/login.php
+++ web/php-project/login.php
@@ -25,25 +25,28 @@
     require 'db_runner.php';
   ?>
   
-  <form class="form_container" action="createaccount.php">
-    <button type="submit">Create Account</button>
-  </form>
-  <br>
-  <form class="form_container" action="index.php?active=HOME" method="post">
-    <div class="imgcontainer">
-      <img src="https://render.fineartamerica.com/images/rendered/default/poster/8.000/8.000/break/images/artworkimages/medium/1/us-army-helmet-4-of-hearts-playing-card-drawing-aloysius-patrimonio.jpg" alt="logo" class="avatar">
-    </div>
+  <div id="login_box">
+    <p>WELCOME TO HOBBY HEROES</p>
+    <form class="form_container" action="createaccount.php">
+      <button type="submit">Create Account</button>
+    </form>
+    <br>
+    <form class="form_container" action="index.php?active=HOME" method="post">
+      <div class="imgcontainer">
+        <img src="https://render.fineartamerica.com/images/rendered/default/poster/8.000/8.000/break/images/artworkimages/medium/1/us-army-helmet-4-of-hearts-playing-card-drawing-aloysius-patrimonio.jpg" alt="logo" class="avatar">
+      </div>
 
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
 
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required><br>
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required><br>
 
-      <button type="submit">Login</button>
-    </div>
-  </form>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  </div>
 
   <?php
     if (isset($_GET['attempt'])) {  
