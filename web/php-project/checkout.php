6aaa4adc10156f3333954af2194aea5d9c429d7d
rifeconrad <rif16001@byui.edu>
2019-06-06 13:23:49 -0400

    commit

--- web/php-project/checkout.php
+++ web/php-project/checkout.php
@@ -17,14 +17,16 @@
 		else if (!isset($_SESSION['cart']) || !empty($_SESSION['cart'])) {
 	?>
 
-	<form action="checkout.php?active=CHECKOUT&purchase=confirmed" method="post">
-		First Name <input type="text" name="first_name" required></input><br>
-		Last Name <input type="text" name="last_name" required></input><br>
-		Street <input type="text" name="street" required></input><br>
-		City <input type="text" name="city" required></input><br>
-		State <input type="text" name="state" required></input><br>
-		<button type="submit">Confirm Purchase</button>
-	</form>
+	<div class="login_box">
+		<form class="form_container" action="checkout.php?active=CHECKOUT&purchase=confirmed" method="post">
+			First Name <input type="text" name="first_name" required></input><br>
+			Last Name <input type="text" name="last_name" required></input><br>
+			Street <input type="text" name="street" required></input><br>
+			City <input type="text" name="city" required></input><br>
+			State <input type="text" name="state" required></input><br>
+			<button type="submit">Confirm Purchase</button>
+		</form>
+	</div>
 
 	<?php
 		} else {
