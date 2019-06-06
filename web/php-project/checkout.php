51da31f2302e928ac0430e43cfebc794adbc2da2
rifeconrad <rif16001@byui.edu>
2019-06-06 13:26:41 -0400

    commit

--- web/php-project/checkout.php
+++ web/php-project/checkout.php
@@ -21,12 +21,12 @@
 
 	<div class="login_box">
 		<form class="form_container" action="checkout.php?active=CHECKOUT&purchase=confirmed" method="post">
-			First Name <input type="text" name="first_name" required></input><br>
-			Last Name <input type="text" name="last_name" required></input><br>
-			Street <input type="text" name="street" required></input><br>
-			City <input type="text" name="city" required></input><br>
-			State <input type="text" name="state" required></input><br>
-			<button type="submit">Confirm Purchase</button>
+			<span class="ind">First Name</span> <input type="text" name="first_name" required></input><br>
+			<span class="ind">Last Name</span> <input type="text" name="last_name" required></input><br>
+			<span class="ind">Street</span> <input type="text" name="street" required></input><br>
+			<span class="ind">City</span> <input type="text" name="city" required></input><br>
+			<span class="ind">State</span> <input type="text" name="state" required></input><br>
+			<button class="form_button" type="submit">Confirm Purchase</button>
 		</form>
 	</div>
 
